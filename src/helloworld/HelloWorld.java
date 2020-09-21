8658cad59cad36bb5cf70b9cd9da9916df3a7105
Sebastian <juans.gomez.2019@upb.edu.co>
2020-09-21 17:19:07 -0500

    Primer commit

---
+++ src/helloworld/HelloWorld.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloworld;
+
+/**
+ *
+ * @author Juan Sebastian Gomez 
+ * Primer Codigo con Java en NetBeans, Hola Mundo.
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hola Mundo");
+    }
+    
+}
